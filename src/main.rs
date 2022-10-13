0733bf71fa04087c3302c59ef1fcfdbd0b799d7b
Devon Knight <devknight324@gmail.com>
2022-10-13 18:01:37 -0400

    Completed Function 3, simulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::i32;
+
 fn main() {
     println!("Welcome to the Rabbit University Safety Testing Suite (RUSTs)");
 
@@ -71,8 +73,19 @@ fn validate_simulation() {
 }
 
 /// Simulate the rabbit population
-fn simulate(starting_rabbits: i128) -> i32 {
-    todo!()
+fn simulate(starting_rabbits: i128) -> i32 {  
+    let mut days: i32 = 0;
+    let mut rabbits: i128 = starting_rabbits;
+    while rabbits > 1{
+        if rabbits % 2 != 0{
+            rabbits = (3 * rabbits) + 1;
+        }
+        else{
+            rabbits = rabbits / 2;
+        }
+        days += 1;
+    }
+    return days;
 }
 
 #[cfg(test)]
